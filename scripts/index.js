c011cae94120a6513095ba881597d37723ef5a9d
Eli Spice <toxicmaroon0@gmail.com>
2023-09-08 08:50:49 +1200

    Responsive Navbar application V0.1.0

--- scripts/index.js
+++ scripts/index.js
@@ -2,3 +2,12 @@
 import "@popperjs/core"
 import "bootstrap"
 
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function phoneScreenToggle() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
