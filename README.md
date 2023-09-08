836569bb2fc0e30690e3cc27562e5e81aff77613
Eli Spice <toxicmaroon0@gmail.com>
2023-09-08 14:34:52 +1200

    Update V0.2.0

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # AS91903-Repo-19041
 A website created for the NCEA standard 91903 by the student 19041 at school code 288
+
+# Changes
+Responsive taskbar integration V1.0.0
+Light/Dark mode integration V0.1.0
--- scripts/index.js
+++ scripts/index.js
@@ -2,3 +2,8 @@
 import "@popperjs/core"
 import "bootstrap"
 
+//Dark mode toggler js code//
+const checkbox = document.getElementById("checkbox")
+checkbox.addEventListener("change", () => {
+  document.body.classList.toggle("dark")
+})
