510dbc31f72da7019e15999bad20d7890acbeb1b
Eli Spice <toxicmaroon0@gmail.com>
2023-09-09 15:44:29 +1200

    Update V0.3.2-2

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,11 @@
-//Run on site load
-const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
-
-if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
-
-    if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
-    }
-}
 //Variables and constants
 var nav = document.getElementById("navBar");
 var dropdownMenu01 = document.getElementById("dropMenu01");
 var dropdownMenu02 = document.getElementById("dropMenu02");
 var dropdownMenu03 = document.getElementById("dropMenu03");
-const checkboxState = document.querySelector('.checkbox input[type="checkbox"]');
+const checkbox = document.getElementById("darkLightToggle");
 
 //Dark mode toggler js code//
-const checkbox = document.getElementById("darkLightToggle");
 checkbox.addEventListener("change", () => {
   document.body.classList.toggle("dark");
   nav.classList.toggle("bg-light");
@@ -27,16 +16,3 @@ checkbox.addEventListener("change", () => {
   dropdownMenu02.classList.toggle("dark");
   dropdownMenu03.classList.toggle("dark");
 });
-
-//Set theme (In local storage, for persistance and dark mode browser extensions)
-
-function localStorThemeTggl(e){
-  if (e.target.checked){
-    document.documentElement.setAttribute('data-theme', 'dark');
-  localStorage.setItem('theme', 'dark');
-}
-else {
-    document.documentElement.setAttribute('data-theme', 'light');
-  localStorage.setItem('theme', 'light');
-}    
-}
